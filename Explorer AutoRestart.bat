6e5643684743fd8519bc61bc04689edd0de863fd
Yonatan Israeli <yonirisraeli@gmail.com>
2024-12-19 18:48:47 -0500

    Update Explorer AutoRestart.bat

--- Explorer AutoRestart.bat
+++ Explorer AutoRestart.bat
@@ -2,10 +2,11 @@
 setlocal
 title Explorer AutoRestart
 echo Program Name: Explorer AutoRestart
-echo Version: 1.0.8
-echo Developer: @YonatanReuvenIsraeli
-echo Website: https://www.yonatanreuvenisraeli.dev
+echo Version: 1.0.9
 echo License: GNU General Public License v3.0
+echo Developer: @YonatanReuvenIsraeli
+echo GitHub: https://github.com/YonatanReuvenIsraeli
+echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli 
 net user > nul 2>&1
 if not "%errorlevel%"=="0" goto "InWindowsRecoveryEnvironment"
 goto "Start"
